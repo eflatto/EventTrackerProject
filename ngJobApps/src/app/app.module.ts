252748601b4defdef1091603b0606364a6fb759c
Angel Garza <gustavo94manga@gmail.com>
2023-04-29 19:44:26 -0500

    create login component

--- ngJobApps/src/app/app.module.ts
+++ ngJobApps/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
