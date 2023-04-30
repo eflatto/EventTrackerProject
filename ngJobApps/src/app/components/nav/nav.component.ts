82057b75e2b7cb4897450594d20faf3db986110c
Angel Garza <gustavo94manga@gmail.com>
2023-04-29 21:44:20 -0500

    update app routing module

--- ngJobApps/src/app/components/nav/nav.component.ts
+++ ngJobApps/src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+
+  isCollapsed = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  loggedIn(): boolean {
+    return this.auth.checkLogin();
+  }
+
+  logout() {
+    console.log('Logging out.');
+    this.auth.logout();
+    this.router.navigateByUrl('/home');
+  }
+
+
 
 }
